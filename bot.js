1ac0600e1185a5da06d272ac4d2d329a79326b51
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-01 23:58:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,4 +6,20 @@ const prefix = "-"
 client.on('message', msg => {   if (msg.content === '.') {     msg.reply('وعليكم السلام');   } });
 
 
+client.on("message", m => {
+  if (m.content === "help") {
+    let Dashboard = "رابط موقع البوت";
+    var addserver ="رابط اضافت البوت ";
+    var SUPPORT = "رابط سيرفر البوت هنا";
+    let embed = new Discord.RichEmbed().setTitle(`Helpful Links`)
+      .setDescription(`                                                                                                               
+**[Add To Your Server ](${addserver})**    
+**[Dashboard](${Dashboard})**
+**[ Server Support](${SUPPORT})**`);
+    m.react("✅");
+    m.author.send(embed);
+  }
+});
+
+
 client.login(process.env.BOT_TOKEN);
