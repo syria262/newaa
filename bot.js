8ca6996c9ac7de0d1b2ff9bb62279a5746a56b65
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-01 23:20:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- 
+const prefix = "-"
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as [ ${client.user.tag}! ]`);
+  console.log('[           BOT IS ONLINE         ]') 
 });
- 
  
 client.on('message', message => {
                                 if(!message.channel.guild) return;
