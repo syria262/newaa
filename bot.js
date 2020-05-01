fc002d84f8c06f3bf3090e08d45e539d45dc0833
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-01 23:40:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,11 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "-"
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setActivity('', { type: 'hi' })
-});
-
 client.on('message', msg => {   if (msg.content === 'السلام عليكم') {     msg.reply('وعليكم السلام');   } });
 
 client.login(process.env.BOT_TOKEN);
