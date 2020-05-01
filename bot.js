a84a98ece7760374af329d48a9bb6f3ad63b70b5
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-01 20:09:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,25 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "-"
+ 
 client.on('ready', () => {
-  console.log(`Logged in as [ ${client.user.tag}! ]`);
-  console.log('[           BOT IS ONLINE         ]') 
+  console.log(`Logged in as ${client.user.tag}!`);
 });
-
-client.login("Your Bot Token");
+ 
+ 
+client.on('message', message => {
+                                if(!message.channel.guild) return;
+                        if (message.content.startsWith(prefix + 'ping')) {
+                            if(!message.channel.guild) return;
+                            var msg = `${Date.now() - message.createdTimestamp}`
+                            var api = `${Math.round(client.ping)}`
+                            if (message.author.bot) return;
+                        let embed = new Discord.RichEmbed()
+                        .setAuthor(message.author.username,message.author.avatarURL)
+                        .setColor('RANDOM')
+                        .addField('**Time Taken:**',msg + " ms 📶 ")
+                        .addField('**WebSocket:**',api + " ms 📶 ")
+         message.channel.send({embed:embed});
+                        }
+   });
+ 
+client.login('token');
