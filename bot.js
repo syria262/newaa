6ee0dbd84f94b8312eb6935ec803c7a5e309f9fe
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-02 00:06:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,16 @@ const prefix = "-"
 
 client.on('message', msg => {   if (msg.content === '.') {     msg.reply('وعليكم السلام');} });
 
+client.on('message', message => {   
+     if (message.content === prefix+'ping') {
+      const embed = new Discord.RichEmbed()
+ 
+  .setColor("RANDOM")
+  .addField('``سرعة أتصال الــبوت`` ' , `${Date.now() - message.createdTimestamp}` + ' ms`')
+ 
 
+  message.channel.sendEmbed(embed);
+    }
+});
 
 client.login(process.env.BOT_TOKEN);
