d9f3926c0898b18b8194cedd19567b67cda8fefb
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-03 18:48:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,5 +2,27 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "-"
 
+client.on("message", message => {
+    var prefix = "#";
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+ if (!args[1]) {
+                                let x5bz1 = new Discord.RichEmbed()
+                                .setDescription("#clear <number>")
+                                .setColor("#0000FF")
+                                message.channel.sendEmbed(x5bz1);
+                            } else {
+                            let messagecount = parseInt(args[1]);
+                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                                                          message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+                            let x5bz2 = new Discord.RichEmbed()
+                                                            .setColor("#008000")
+                                .setDescription(":white_check_mark: | Delete " + args[1] + " Message!")
+                                                                                        message.delete("..");
+                                message.channel.sendEmbed(x5bz2);
+                            }
+                          }
+});
 
 client.login(process.env.BOT_TOKEN);
