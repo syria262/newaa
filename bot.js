445634effbf6c53ff81924200e06b9895aefa9ac
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-04 19:53:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,5 +3,40 @@ const client = new Discord.Client();
 const moment = require ("moment");
 const prefix = "!";
 
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'help')) { 
+    let embed = new Discord.RichEmbed()
+    .setColor("BLACK")
+    .setAuthor(client.user.tag,client.user.avatarURL)
+    .setDescription(`Please Chose
+🔴Help in channel
+🔵Help in privte
+`)
+  message.channel.send(embed).then(async msg => {
+      msg.react("🔴")
+      msg.react("🔵")
+      
+ const doma= msg.createReactionCollector((reaction, user) => reaction.emoji.name == "🔴" && user.id == message.author.id, {time: 86400000})
+ const domaa = msg.createReactionCollector((reaction, user) => reaction.emoji.name == "🔵" && user.id == message.author.id, {time: 86400000})
+ 
+ doma.on("collect",async r => {
+   await r.remove(message.author)
+   let embe = new Discord.RichEmbed()
+   .setColor("BLACK")
+   .setAuthor(client.user.tag,client.user.avatarURL)
+.setDescription(`الرساله `)
+   msg.edit(embe)
+ })
+      domaa.on("collect", async r => {
+      await r.remove(message.author)
+        let embe = new Discord.RichEmbed()
+        .setColor("BLACK")
+        .setAuthor(client.user.tag,client.user.avatarURL)
+        .setDescription(`الرساله `)
+      message.author.send(embe)
+      })
+    })
+    }
+})
 
 client.login(process.env.BOT_TOKEN);
