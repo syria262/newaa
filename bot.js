f2c463b18761faba76be5889389204ee30c4363a
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-04 20:12:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,22 +3,74 @@ const client = new Discord.Client();
 const moment = require ("moment");
 const prefix = "!";
 
+client.on("message", message => {
+	var prefix = "*";
+ if (message.content === "*help") {
+  const embed = new Discord.RichEmbed()  
+      .setColor("#8325c0") 
+      .setDescription(`
+	  
+	       Help Commands: 
 
-  client.on('message', async message => {
-  if(message.content.startsWith(prefix + "sugg")) {
-  await  message.channel.send(`اكتب اقتراحك الان`)
-    let filter = m => m.author.id === message.author.id
-      var text = '';
-        let sugsa = message.channel.awaitMessages(filter, { max: 1, time: 60000})
-          .then(co => {
-            text = co.first().content
+			 
+${prefix}public ⥨ الاوامر العامة
 
-              message.channel.send(`تم حفظ اقتراحك الرجاء انتضار الرد من قبل الاداره`)
-                client.channels.get("705203120334045194").send(`${message.author.username}'s sug => ${text}`)
+${prefix}admin ⥨ اوامر الادارة
+			 
+${prefix}games ⥨ اوامر الالعاب
 
-              })
-            }
-          })
- 
+${prefix}music ⥨ اوامر الموسيقى
+
+Other Commands:
+
+
+${prefix}invite ⥨ لدعوة البوت الى سيرفرك
+
+${prefix}support ⥨ لدخول سيرفر الدعم
+
+
+	  `)
+   message.channel.sendEmbed(embed)
+    
+   }
+   }); 
+   
+
+   client.on("message", message => {
+ if (message.content === "*public") {
+        message.react("📫")
+	           message.react("✅")
+  const embed = new Discord.RichEmbed() 
+      .setColor("#8325c0")
+      .setThumbnail(message.author.avatarURL)
+      .setDescription(`
+	  
+الاوامــر الــعـــامـــة
+
+⤠ *invite ⥨ لدعوة البوت الى سيرفرك
+⤠ *server ⥨ معلومات عن السيرفر                      
+⤠ *say ⥨ البوت يردد كلامك         
+⤠ *setcolor ⥨ عشان تغير لونك ملاحظة لازم تحط رقم اللون                                          
+⤠ *bot ⥨ معلومات عن البوت
+⤠ *ping ⥨ لمعرفه سرعه البوت
+⤠ *members ⥨ معلومات عن الاعضاء
+⤠ *emojilist ⥨ لعرض الايموجي حقت السيرفر
+⤠ *id ⥨ لمعرفة معلومات حسابك
+⤠ *avatar ⥨ لاعطائك صورة الشخص اللي منشنته مع الرابط
+⤠ *link ⥨ يعطيك رابط انفايت للسيرفر اللي انت فيه
+⤠ *trans <language> <any thing> ⥨ يترجم لك الي تبيه من اي لغة
+⤠ *short ⥨ لاختصار الروابط
+⤠ *embed ⥨ كتابة كلامك داخل امبد
+⤠ *tag ⥨ يكتب لك الكلمة بشكل جميل وكبير
+⤠ *contact ⥨ لارسال رسالة لصاحب البوت
+⤠ *support ⥨ لدخول سيرفر دعم البوت
+	  
+`)
+
+
+message.author.sendEmbed(embed)
+
+}
+});
 
 client.login(process.env.BOT_TOKEN);
