b27689a59b3ac84ab5ee196ff24b6d22510b5c8f
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-06 00:59:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,23 +3,6 @@ const client = new Discord.Client();
 const moment = require ("moment");
 const prefix = "!";
 
-client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-       if (message.content.startsWith(prefix + "avatar")) {
-           var mentionned = message.mentions.users.first();
-           message.channel.send(·تفضل رابط الصورة الي طلبتها  : `);
-    var MsH;
-      if(mentionned){
-          var MsH = mentionned;
-      } else {
-          var MsH = message.author;
-         
-      }
-          message.channel.send(MsH.avatarURL);
-      }
-         
-});
+
 
 client.login(process.env.BOT_TOKEN);
