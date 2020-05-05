bcce1a815acb877bb9350edea668ae830ee02cd9
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-06 00:36:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,80 +3,22 @@ const client = new Discord.Client();
 const moment = require ("moment");
 const prefix = "!";
 
-client.on('message' , message => {
-     const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
- if(message.content.split(' ')[0].toLowerCase() == prefix + 'id') {
-     const millis = new Date().getTime() - message.guild.createdAt.getTime();
-    const now = new Date();
-    const createdAt = millis / 1000 / 60 / 60 / 24;
-    if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
-    let user = message.mentions.users.first() || message.author;
-    message.delete();
-   
-var men = message.mentions.users.first();
- var heg;
- if(men) {
-     heg = men
- } else {
-     heg = message.author
- }
-var mentionned = message.mentions.members.first();
-  var h;
- if(mentionned) {
-     h = mentionned
- } else {
-     h = message.member
- }
-
-    let game;
-    if (user.presence.game === null) {
-        game = 'None.';
-    } else {
-        game = user.presence.game.name;
-    }
-    let messag;
-    if (user.lastMessage === null) {
-        messag = 'None.';
-    } else {
-        messag = user.lastMessage;
-    }
-    let status;
-    if (user.presence.status === 'online') {
-        status = 'Online';
-    } else if (user.presence.status === 'dnd') {
-        status = 'DND';
-    } else if (user.presence.status === 'idle') {
-        status = 'Idle';
-    } else if (user.presence.status === 'offline') {
-        status = 'Offline';
-    }
-    if (user.presence.status === 'offline') {
-        stat = 0x000000;
-    } else if (user.presence.status === 'online') {
-        stat = 0x00AA4C;
-    } else if (user.presence.status === 'dnd') {
-        stat = 0x9C0000;
-    } else if (user.presence.status === 'idle') {
-        stat = 0xF7C035;
-    }
-    moment.locale('En-ly');
-                    message.guild.fetchInvites().then(invs => {
-      let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
-      let Invites = invs.filter(i => i.inviter.id);
-      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-    const embed = new Discord.RichEmbed()
-
-  
-  .addField('معلوماتك في الديسكورد  : ', `اسمك: ${user.username}\n تاقك: #${user.discriminator}\nايديك : ${user.id} \nدخولك للديسكورد من : ${moment(heg.createdTimestamp).fromNow()}\nوضعك : ${status}`,true)
-  .addField('معلوماتك في السيرفر :', `دخولك للسيرفر من :  ${moment(h.joinedAt).fromNow()} \n عدد دعواتك :  ${inviteCount} Invite(s) \nرتبك : `+message.guild.members.get(user.id).roles.array(role => role.name).slice(1).join(', '))
-  .setAuthor(`${user.username}`, user.displayAvatarURL)
-  .setColor('RANDOM')
-    .setThumbnail(user.displayAvatarURL)
-    message.channel.send({embed})
-  .catch(e => logger.error(e));
- })
-}
- }); 
-
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+       if (message.content.startsWith(prefix + "avatar")) {
+           var mentionned = message.mentions.users.first();
+           message.channel.send(`ًںڈ· ${message.author.username} avatar URL : `);
+    var MsH;
+      if(mentionned){
+          var MsH = mentionned;
+      } else {
+          var MsH = message.author;
+         
+      }
+          message.channel.send(MsH.avatarURL);
+      }
+         
+});
 client.login(process.env.BOT_TOKEN);
