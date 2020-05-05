489de79718dd744cf9523c5aa865ff5a9387e98d
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-05 20:17:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,21 +3,80 @@ const client = new Discord.Client();
 const moment = require ("moment");
 const prefix = "!";
 
-client.on('message', message => {
-                                if(!message.channel.guild) return;
-                        if (message.content.startsWith(prefix + 'ping')) {
-                            if(!message.channel.guild) return;
-                            var msg = `${Date.now() - message.createdTimestamp}`
-                            var api = `${Math.round(client.ping)}`
-                            if (message.author.bot) return;
-                        let embed = new Discord.RichEmbed()
-                        .setAuthor(message.author.username,message.author.avatarURL)
-                        .setColor('RANDOM')
-                        .addField('**الوقت المستغرق:**',msg + " ms 📶 ")
-                        .addField('**سرعة اتصال البوت:**',api + " ms 📶 ")
-         message.channel.send({embed:embed});
-                        }
-   });
- 
+client.on('message' , message => {
+     const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+ if(message.content.split(' ')[0].toLowerCase() == prefix + 'id') {
+     const millis = new Date().getTime() - message.guild.createdAt.getTime();
+    const now = new Date();
+    const createdAt = millis / 1000 / 60 / 60 / 24;
+    if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
+    let user = message.mentions.users.first() || message.author;
+    message.delete();
+   
+var men = message.mentions.users.first();
+ var heg;
+ if(men) {
+     heg = men
+ } else {
+     heg = message.author
+ }
+var mentionned = message.mentions.members.first();
+  var h;
+ if(mentionned) {
+     h = mentionned
+ } else {
+     h = message.member
+ }
+
+    let game;
+    if (user.presence.game === null) {
+        game = 'None.';
+    } else {
+        game = user.presence.game.name;
+    }
+    let messag;
+    if (user.lastMessage === null) {
+        messag = 'None.';
+    } else {
+        messag = user.lastMessage;
+    }
+    let status;
+    if (user.presence.status === 'online') {
+        status = 'Online';
+    } else if (user.presence.status === 'dnd') {
+        status = 'DND';
+    } else if (user.presence.status === 'idle') {
+        status = 'Idle';
+    } else if (user.presence.status === 'offline') {
+        status = 'Offline';
+    }
+    if (user.presence.status === 'offline') {
+        stat = 0x000000;
+    } else if (user.presence.status === 'online') {
+        stat = 0x00AA4C;
+    } else if (user.presence.status === 'dnd') {
+        stat = 0x9C0000;
+    } else if (user.presence.status === 'idle') {
+        stat = 0xF7C035;
+    }
+    moment.locale('En-ly');
+                    message.guild.fetchInvites().then(invs => {
+      let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
+      let Invites = invs.filter(i => i.inviter.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+    const embed = new Discord.RichEmbed()
+
+  
+  .addField('Discord Info : ', `Name : ${user.username}\n Discriminator: #${user.discriminator}\nID : ${user.id} \nJoinedDiscord : ${moment(heg.createdTimestamp).fromNow()}\nBot :  ${user.bot}\nPlaying : ${game}\nStatus : ${status}`,true)
+  .addField('Server Info :', `LastMessage : ${messag}\nJoined :  ${moment(h.joinedAt).fromNow()} \n Invites :  ${inviteCount} Invite(s) \nRoles : `+message.guild.members.get(user.id).roles.array(role => role.name).slice(1).join(', '))
+  .setAuthor(`${user.username}`, user.displayAvatarURL)
+  .setColor('#36393e')
+    .setThumbnail(user.displayAvatarURL)
+    message.channel.send({embed})
+  .catch(e => logger.error(e));
+ })
+}
+ }); 
 
 client.login(process.env.BOT_TOKEN);
