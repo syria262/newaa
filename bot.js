14c38440933b9f765ea83ebe85e557e8422b5b21
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-06 00:51:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,50 +3,6 @@ const client = new Discord.Client();
 const moment = require ("moment");
 const prefix = "!";
 
- client.on("message",message => {
-if(message.author.bot) return;
-if(!message.content.startsWith(prefix)) return;
-  if(message.content.startsWith(prefix + "avatar")){
-const mention = message.mentions.users.first()
 
-if(!mention) return console.log("") 
-let embed = new Discord.RichEmbed()
-.setColor("BLACK")
-.setAuthor(`${mention.username}#${mention.discriminator}`,`${mention.avatarURL}`) 
-.setTitle("Avatar Link")
-.setURL(`${mention.avatarURL}`)
-.setImage(`${mention.avatarURL}`)
-.setFooter(`Requested By ${message.author.tag}`,`${message.author.avatarURL}`)    
-    message.channel.send(embed)
-}
-})
 
-client.on("message", message => {
-  if(message.author.bot) return;
-  if(!message.content.startsWith(prefix)) return;
-  if(message.content.startsWith(prefix + "avatar server")) {
-    let doma = new Discord.RichEmbed()
-    .setColor("BLACK")
-    .setAuthor(message.guild.name, message.guild.iconURL)
-    .setTitle("Avatar Link")
-    .setURL(message.guild.iconURL)
-    .setImage(message.guild.iconURL)
-    .setFooter(`Requested By ${message.author.tag}`, message.author.avatarURL)
-    message.channel.send(doma)
-  } else if(message.content.startsWith(prefix + "avatar")) {
-    let args = message.content.split(" ")[1]
-var avt = args || message.author.id;    
-    client.fetchUser(avt).then(user => {
-     avt = user;
-  let embed = new Discord.RichEmbed() 
-  .setColor("BLACK")
-  .setAuthor(`${avt.tag}`, avt.avatarURL)
-  .setTitle("Avatar Link")
-  .setURL(avt.avatarURL)
-  .setImage(avt.avatarURL)
-  .setFooter(`Requested By ${message.author.tag}`, message.author.avatarURL)
-  message.channel.send(embed) 
-    })
-  }
-}) 
 client.login(process.env.BOT_TOKEN);
