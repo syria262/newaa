c6d1599f80eb9ab0b32aec11c1f598ff6956543e
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-07 19:46:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,5 +3,33 @@ const client = new Discord.Client();
 const moment = require ("moment");
 const prefix = "!";
 
+client.on('message', message => {
+
+    if (message.content === "!mc") {
+                        if(!message.channel.guild) return message.reply(' هذا الامر فقط للسيرفرات !!');
+
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' [user] تبي ميوت صح ؟  ');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: false
+
+           }).then(() => {
+               message.reply("لقد تم تقفيل الشات ✅ ")
+           });
+             }
+if (message.content === "!unmc") {
+    if(!message.channel.guild) return message.reply(' هذا الامر فقط للسيرفرات !!');
+
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('ليس لديك صلاحيات');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: true
+
+           }).then(() => {
+               message.reply("لقد تم فتح الشات✅")
+           });
+             }
+
+
+
+});
 
 client.login(process.env.BOT_TOKEN);
