aa270d1bed93248ce99c21d7d2f8fcf5181c6a7e
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-07 20:35:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,6 @@ const prefix = "!";
 client.on('message', message => {
   if (message.author.x5bz) return;
   if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
   command = command.slice(prefix.length);
 
   let args = message.content.split(" ").slice(1);
