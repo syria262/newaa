8a162909128352f6c1e8e8c162fbb37769acad96
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-07 21:34:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,5 +4,21 @@ const moment = require ("moment");
 const prefix = "!";
 
 
+client.on('message', message => {
+if (message.content.startsWith(prefix+"ct")) {
+    var args = message.content.split(" ").slice(1);
+    var argrst = args.join(' ');
+                message.guild.createChannel(`${argrst}`, 'text')
+      }
+});
+
+client.on('message', message => {
+if (message.content.startsWith(prefix+"cv")) {
+    var args = message.content.split(" ").slice(1);
+    var argrst = args.join(' ');
+                message.guild.createChannel(`${argrst}`,'voice')
+          
+        }
+});
 
 client.login(process.env.BOT_TOKEN);
