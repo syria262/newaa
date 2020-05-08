821a0696ef8a82df1d9911964f5b1c87099eeadd
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-08 19:55:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,6 @@ if (message.content.startsWith("!ip")) {
 let auto = new Discord.RichEmbed()
 .setDescription(`تفضل اي بي ال سيرفر : 51.38.84.69:25615`)
 message.channel.send(auto)
-});
+}})
   
 client.login(process.env.BOT_TOKEN);
