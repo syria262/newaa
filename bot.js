efc20a686f12713923dc20b470c7cc5e7c3babab
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-09 01:12:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -455,58 +455,6 @@ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('تب�
 });
 
      
-      message.guild.member(user).kick(7, user);
-     
-      const banembed = new Discord.RichEmbed()
-      .setAuthor('Kicked !', user.displayAvatarURL)
-      .setColor("RANDOM")
-      .setTimestamp()
-      .addField("الشخص:",  `[ + ${user.tag} + ]`)
-      .addField("من:", `[  + ${message.author.tag} +  ]`)
-      .addField("السبب:", `[ + ${reason} +  ]`)
-      client.channels.get("492583022982463500").send({embed : banembed})
-    }
-    });
-
-    client.on('message', message => {
-     if (message.author.x5bz) return;
-      if (!message.content.startsWith(prefix)) return;
-     
-      let command = message.content.split(" ")[0];
-      command = command.slice(prefix.length);
-     
-      let args = message.content.split(" ").slice(1);
-     
-      if (command == "ban") {
-                   if(!message.channel.guild) return message.reply('** This command only for servers**');
-             
-      if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
-      if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
-      let user = message.mentions.users.first();
-      let reason = message.content.split(" ").slice(2).join(" ");
-      /*let b5bzlog = client.channels.find("name", "5bz-log");
-     
-      if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
-      if (message.mentions.users.size < 1) return message.reply("**لازم تمنشن شخص**");
-      if(!reason) return message.reply ("**اكتب سبب الطرد**");
-      if (!message.guild.member(user)
-      .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
-     
-      message.guild.member(user).ban(7, user);
-     
-      const banembed = new Discord.RichEmbed()
-      .setAuthor(`BANNED!`, user.displayAvatarURL)
-      .setColor("RANDOM")
-      .setTimestamp()
-      .addField("**الشخص:**",  '**[ ' + `${user.tag}` + ' ]**')
-      .addField("**من:**", '**[ ' + `${message.author.tag}` + ' ]**')
-      .addField("**السبب:**", '**[ ' + `${reason}` + ' ]**')
-      message.channel.send({
-        embed : banembed
-      })
-    }
-    });
-
 client.on('message', message => {
 if (message.content.startsWith(prefix+"ct")) {
     var args = message.content.split(" ").slice(1);
