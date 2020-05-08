ba61939fcca22965426b3d51e7563b53a955b6a4
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-08 19:53:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,10 @@ const moment = require ("moment");
 const prefix = "!";
 
 client.on("message", message => {
-    if (msg.content === '!ip') {
-if (new Discord.RichEmbed) (setDescription(`تفضل`)
-message.channel.send("تفضل1")
-
+if (message.content.startsWith("!ip")) {
+let auto = new Discord.RichEmbed()
+.setDescription(`تفضل اي بي ال سيرفر : 51.38.84.69:25615`)
+message.channel.send(auto)
+});
   
 client.login(process.env.BOT_TOKEN);
