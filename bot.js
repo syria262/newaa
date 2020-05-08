2b6a33621d8f6d6f947a6e8bdb964fb1f9f54a2f
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-08 20:02:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ const prefix = "!";
 client.on("message", message => {
 if (message.content.startsWith("!ip")) {
 let auto = new Discord.RichEmbed()
-.setDescription(``تفضل اي بي ال سيرفر : `51.38.84.69:25615`)
-.setColor("FF9900")
+.setDescription(`تفضل اي بي ال سيرفر : `51.38.84.69:25615`)
+.setColor("FFCC00")
 message.channel.send(auto)
 }})
   
