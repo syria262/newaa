5b11b340d4eef82066cecefa65c32f22eaab2d53
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-08 19:37:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,46 +3,11 @@ const client = new Discord.Client();
 const moment = require ("moment");
 const prefix = "!";
 
-// Requires Manager from discord-giveaways
-const { GiveawaysManager } = require("discord-giveaways");
-// Starts updating currents giveaways
-const manager = new GiveawaysManager(client, {
-    storage: "./giveaways.json",
-    updateCountdownEvery: 10000,
-    default: {
-        botsCanWin: false,
-        exemptPermissions: [ "MANAGE_MESSAGES", "ADMINISTRATOR" ],
-        embedColor: "#FF0000",
-        reaction: "🎉"
-    }
-});
-// We now have a giveawaysManager property to access the manager everywhere!
-client.giveawaysManager = manager;
- 
-client.on("ready", () => {
-    console.log("I'm ready !");
-});
-
-client.on("message", (message) => {
- 
-    const ms = require("ms"); // npm install ms
-    const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
- 
-    if(command === "start-giveaway"){
-        // g!start-giveaway 2d 1 Awesome prize!
-        // will create a giveaway with a duration of two days, with one winner and the prize will be "Awesome prize!"
- 
-        client.giveawaysManager.start(message.channel, {
-            time: ms(args[0]),
-            prize: args.slice(2).join(" "),
-            winnerCount: parseInt(args[1])
-        }).then((gData) => {
-            console.log(gData); // {...} (messageid, end date and more)
-        });
-        // And the giveaway has started!
-    }
-});
+client.on("message", message => {
+if (message.content.startsWith("!ip")
+let auto = new Discord.RichEmbed()
+.setDescription(`تفضل`)
+message.channel.send("تفضل1")
 
 
 client.login(process.env.BOT_TOKEN);
