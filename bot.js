49a56b3a9177874418ff60befbb1d13c17a2747b
syria262 <53955666+syria262@users.noreply.github.com>
2020-05-09 01:17:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,6 +102,22 @@ client.on('message', function(msg) {
     }
   });
 
+client.on('message', message => {
+                                if(!message.channel.guild) return;
+                        if (message.content.startsWith(prefix + 'ping')) {
+                            if(!message.channel.guild) return;
+                            var msg = `${Date.now() - message.createdTimestamp}`
+                            var api = `${Math.round(client.ping)}`
+                            if (message.author.bot) return;
+                        let embed = new Discord.RichEmbed()
+                        .setAuthor(message.author.username,message.author.avatarURL)
+                        .setColor('RANDOM')
+                        .addField('**الوقت المستغرق:**',msg + " ms 📶 ")
+                        .addField('**سرعة اتصال البوت:**',api + " ms 📶 ")
+         message.channel.send({embed:embed});
+                        }
+   });
+
 
 
 client.login(process.env.BOT_TOKEN);
